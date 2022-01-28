8e29addca92002d23285a1e5bb85a3738531435b
Kayla Wallace <ktwallace511@gmail.com>
2022-01-28 23:25:31 +0000

    Orb projectile moves in sine curve

--- Assets/Scripts/Orb.cs
+++ Assets/Scripts/Orb.cs
@@ -7,11 +7,22 @@ public class Orb : MonoBehaviour
     public float speed = 15f;
     public Rigidbody rb;
     public GameObject hitEffect;
+
+    private float size = .1f, freq = 10f;
+    private Vector3 axis;
+    private Vector3 pos;
     
     // Start is called before the first frame update
     void Start()
     {
-        rb.velocity = transform.right * speed;
+        axis = transform.up;
+        pos = transform.position;
+    }
+
+    private void Update()
+    {
+        pos += transform.right * Time.deltaTime * speed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * freq) * size;
     }
 
     private void OnTriggerEnter(Collider other)
